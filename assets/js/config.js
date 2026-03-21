3f563c3744bc492828df74d994680317e38719ad
Saket Pandey <nikhilpandey.pandey9@gmail.com>
2026-03-22 00:16:33 +0530

    ff

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-(function (globalScope) {
-    'use strict';
-
-    function resolveStrapiBaseUrl() {
-        const winUrl = globalScope?.STRAPI_URL;
-        if (typeof winUrl === 'string' && winUrl.trim()) return winUrl.trim();
-
-        const envUrl =
-            typeof process !== 'undefined' && process?.env && typeof process.env.STRAPI_URL === 'string'
-                ? process.env.STRAPI_URL
-                : '';
-        if (envUrl && envUrl.trim()) return envUrl.trim();
-
-        return 'http://localhost:1337';
-    }
-
-    globalScope.resolveStrapiBaseUrl = resolveStrapiBaseUrl;
-})(typeof window !== 'undefined' ? window : globalThis);
-=======
 (function (global) {
   'use strict';
 
@@ -26,7 +6,7 @@
       if (typeof process !== 'undefined' && process && process.env && process.env.STRAPI_URL) {
         return process.env.STRAPI_URL;
       }
-    } catch (_) {}
+    } catch (_) { }
     return '';
   }
 
@@ -36,4 +16,3 @@
 
   global.getStrapiUrl = resolveStrapiUrl;
 })(window);
->>>>>>> pre-develop
